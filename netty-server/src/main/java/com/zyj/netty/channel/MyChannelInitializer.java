906857698d5a363d474823141fbe98f0de3fe652
yjzhang <zhangyijun0229@163.com>
2021-04-02 11:54:49 +0800

    io

---
+++ netty-server/src/main/java/com/zyj/netty/channel/MyChannelInitializer.java
@@ -0,0 +1,9 @@
+package com.zyj.netty.channel;
+/**
+ *@author : zhang yijun
+ *@date : 2021/4/2 10:23
+ *@description : TODO
+ */
+
+public class MyChannelInitializer {
+}
---
+++ netty-server/src/main/java/com/zyj/netty/handler/MySimpleChannelHander.java
@@ -0,0 +1,9 @@
+package com.zyj.netty.handler;
+/**
+ *@author : zhang yijun
+ *@date : 2021/3/30 10:02
+ *@description : TODO
+ */
+
+public class MySimpleChannelHander {
+}
